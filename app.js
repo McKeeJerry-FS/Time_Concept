43da9542e1ef73205ef66373e912b6507f99e552
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2025-01-27 10:54:24 -0500

    updates

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const timezone = require('zipcode-to-timezone');
 const ejs = require('ejs');
+const moment = require('moment');
+const moment_tz = require('moment-timezone');
 const app = express();
 const port = 5001;
 app.set('view engine', 'ejs');
@@ -9,9 +11,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
 	res.render('index', { greeting: '' });
 });
-app.post('/greet', (req, res) => {
+app.post('/convert', (req, res) => {
 	const zip = req.body.zip;
+    const dec = moment("2014-12-01T12:00:00Z");
     const tz = timezone.lookup(zip)
+    const time = dec.tz(tz).format('ha z');
 	const greeting = `Timezone for zipcode ${zip} is ${tz}`;
 	res.render('index', { greeting });
 });
